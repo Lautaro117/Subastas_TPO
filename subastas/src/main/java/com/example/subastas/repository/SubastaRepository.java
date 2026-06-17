3b6896df2371646d27415eeb8c40e980995f6c24
Julian Zuchowicki <julianzucho@gmail.com>
2026-06-17 08:23:05 -0300

    Subastas en vivo admin

--- subastas/src/main/java/com/example/subastas/repository/SubastaRepository.java
+++ subastas/src/main/java/com/example/subastas/repository/SubastaRepository.java
@@ -2,9 +2,16 @@ package com.example.subastas.repository;
 
 import com.example.subastas.model.Subasta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface SubastaRepository extends JpaRepository<Subasta, Integer> {
     List<Subasta> findByCategoria(String categoria);
     List<Subasta> findByEstado(String estado);
+
+    @Modifying
+    @Query(value = "SELECT cerrar_subasta(:id)", nativeQuery = true)
+    void cerrarSubasta(@Param("id") Integer id);
 }
