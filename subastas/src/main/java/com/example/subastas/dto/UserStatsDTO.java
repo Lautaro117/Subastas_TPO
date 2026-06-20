52a2a08c7cf6d8a004516ba742e421e67b1fe23f
Julian Zuchowicki <julianzucho@gmail.com>
2026-06-20 14:19:28 -0300

    Estadisticas reactor segun req

--- subastas/src/main/java/com/example/subastas/dto/UserStatsDTO.java
+++ subastas/src/main/java/com/example/subastas/dto/UserStatsDTO.java
@@ -1,5 +1,6 @@
 package com.example.subastas.dto;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import com.example.subastas.model.MedioPago;
@@ -9,15 +10,20 @@ public class UserStatsDTO {
     private int pujasRealizadas;
     private int productosPublicados;
     private int articulosGanados;
+    private BigDecimal importeTotalOfertado;
+    private BigDecimal importeTotalPagado;
     private String categoria;
     private List<MedioPago> mediosPagoHabilitados;
 
     public UserStatsDTO(int subastasParticipadas, int pujasRealizadas, int productosPublicados,
-                        int articulosGanados, String categoria, List<MedioPago> mediosPagoHabilitados) {
+                        int articulosGanados, BigDecimal importeTotalOfertado, BigDecimal importeTotalPagado,
+                        String categoria, List<MedioPago> mediosPagoHabilitados) {
         this.subastasParticipadas = subastasParticipadas;
         this.pujasRealizadas = pujasRealizadas;
         this.productosPublicados = productosPublicados;
         this.articulosGanados = articulosGanados;
+        this.importeTotalOfertado = importeTotalOfertado;
+        this.importeTotalPagado = importeTotalPagado;
         this.categoria = categoria;
         this.mediosPagoHabilitados = mediosPagoHabilitados;
     }
@@ -26,6 +32,8 @@ public class UserStatsDTO {
     public int getPujasRealizadas() { return pujasRealizadas; }
     public int getProductosPublicados() { return productosPublicados; }
     public int getArticulosGanados() { return articulosGanados; }
+    public BigDecimal getImporteTotalOfertado() { return importeTotalOfertado; }
+    public BigDecimal getImporteTotalPagado() { return importeTotalPagado; }
     public String getCategoria() { return categoria; }
     public List<MedioPago> getMediosPagoHabilitados() { return mediosPagoHabilitados; }
 }
